cdceb3f9c78c4bf0811feb807ef34824b65005ea
REX\temp9 <abblyet001@gmail.com>
2026-09-24 17:38:11 +0800

    Deploy: 不喜歡分類固定最後、挖掘配額標示與預估

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "b6b80cfee09751867d849563d5763239",
-"assets/AssetManifest.bin.json": "8f75b5b92d52b358d142cc4f0090866a",
-"assets/AssetManifest.json": "0b3c9d4888bd26aff435466fcd0459b6",
+const RESOURCES = {"assets/AssetManifest.bin": "9459b091925e0582f98295d032d1e4bf",
+"assets/AssetManifest.bin.json": "95bb3f30f15e62bd7f7951c376805471",
+"assets/AssetManifest.json": "1ad22af21548811d8264af74045de9b3",
 "assets/assets/config/app_defaults.yaml": "f52eccc6d8df9e48f90e659277eea80d",
 "assets/assets/data/diary.json": "58e0494c51d30eb3494f7c9198986bb9",
 "assets/assets/data/fitness_entries.json": "58e0494c51d30eb3494f7c9198986bb9",
@@ -34,7 +34,6 @@ const RESOURCES = {"assets/AssetManifest.bin": "b6b80cfee09751867d849563d5763239
 "assets/assets/images/user_profile/user_profile.png": "617cc1b9a125a10347285fb7dfb9d39c",
 "assets/assets/images/yt_tracker/all.png": "7ade7f0f5f1c32d7c32b99630a84f2b5",
 "assets/assets/images/yt_tracker/car.png": "a3f05a0846132a322cec6371b0cde8cf",
-"assets/assets/images/yt_tracker/ChatGPT%2520Image%25202026%25E5%25B9%25B49%25E6%259C%258824%25E6%2597%25A5%2520%25E4%25B8%258B%25E5%258D%258805_31_54.png": "77b9070437c06d7e4d627187313ae389",
 "assets/assets/images/yt_tracker/crypto.png": "a07520a245aeacd1a289a0a14b5cc61f",
 "assets/assets/images/yt_tracker/current_affairs.png": "3eb4af02c9c57c34ca2f17bdac6d9ac9",
 "assets/assets/images/yt_tracker/disliked.png": "f58e3d15145210f52277c41f86abc891",
@@ -44,7 +43,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b6b80cfee09751867d849563d5763239
 "assets/assets/images/yt_tracker/games.png": "f14071bb0d712a0d91051021d00c8e33",
 "assets/assets/images/yt_tracker/knowledge.png": "f9880ef016609295f864adf05fdb4c0d",
 "assets/assets/images/yt_tracker/life.png": "9d687275999ca0c950b80e15ff8c6e5d",
-"assets/assets/images/yt_tracker/music.png": "0025704ee793936e15aec0f17a339eb6",
+"assets/assets/images/yt_tracker/music.png": "77b9070437c06d7e4d627187313ae389",
 "assets/assets/images/yt_tracker/news.png": "b83c931bf121b614218be2f7a6c5839b",
 "assets/assets/images/yt_tracker/study.png": "da8a82a2ab1d412e085976e71571cb9e",
 "assets/assets/images/yt_tracker/travel.png": "37f84956b699a2b7e0fb1b3c465c2ebb",
@@ -68,14 +67,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "b6b80cfee09751867d849563d5763239
 "favicon.png": "aa61c0b19e621cecf4646c189cfab221",
 "favicon.svg": "f389abc7848afd594ce4b368cfdb516e",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "1fa9cb95f7ac9ce3254fc0d0f6d60890",
+"flutter_bootstrap.js": "4ae25a82517ec6f6bcdffec7bfa788d0",
 "icons/Icon-192.png": "240c35e6f89a1a62556d56a6aaa38999",
 "icons/Icon-512.png": "52f5fe4099f4338efb1289d24a7cdfe0",
 "icons/Icon-maskable-192.png": "d5731d79d8a5c02c0fa1774eb475cefc",
 "icons/Icon-maskable-512.png": "eba28d91bd6967c56d4a4cc80cfe0642",
 "index.html": "0625f78d3b277c991bc1c28ed82e8f5c",
 "/": "0625f78d3b277c991bc1c28ed82e8f5c",
-"main.dart.js": "7ce380c4290b4b65cc97cb7bfa21a4bc",
+"main.dart.js": "c738d2b15401e8c38d6c7836f43357a6",
 "manifest.json": "f96ea3fa7f2311b731f99b5f8a07dec5",
 "version.json": "1c0e216e937d0bcb2aea9e7bc937d959"};
 // The application shell files that are downloaded before a service worker can
